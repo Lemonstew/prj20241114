cef3b8b09795bb94a67fb55984de07952274ca4e
Lemonstew <colt4949@naver.com>
2024-11-14 12:32:59 +0900

    fe : 목록의 행 클릭 시 /view/id 로 이동

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -1,21 +1,42 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Table } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
     axios.get("/api/board/list").then((res) => setBoardList(res.data));
   }, []);
 
+  function handleRowClick(id) {
+    navigate(`/view/${id}`);
+  }
+
   return (
     <Box>
       <h3>게시물 목록</h3>
-      {boardList.map((board) => (
-        <li>
-          {board.title}, {board.writer}
-        </li>
-      ))}
+      <Table.Root interactive>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>번호</Table.ColumnHeader>
+            <Table.ColumnHeader>제목</Table.ColumnHeader>
+            <Table.ColumnHeader>작성자</Table.ColumnHeader>
+            <Table.ColumnHeader>작성일시</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {boardList.map((board) => (
+            <Table.Row onClick={() => handleRowClick(board.id)} key={board.id}>
+              <Table.Cell>{board.id}</Table.Cell>
+              <Table.Cell>{board.title}</Table.Cell>
+              <Table.Cell>{board.writer}</Table.Cell>
+              <Table.Cell>{board.inserted}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </Box>
   );
 }
