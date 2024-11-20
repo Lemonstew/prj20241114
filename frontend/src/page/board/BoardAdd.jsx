b1ec887c4f9c2be022461b72fc73bbb68fe9053a
Lemonstew <colt4949@naver.com>
2024-11-20 17:45:27 +0900

    파일 업로드

--- frontend/src/page/board/BoardAdd.jsx
+++ frontend/src/page/board/BoardAdd.jsx
@@ -9,6 +9,7 @@ import { toaster } from "../../components/ui/toaster.jsx";
 export function BoardAdd() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [files, setFiles] = useState([]);
   const [progress, setProgress] = useState(false);
 
   const navigate = useNavigate();
@@ -17,7 +18,7 @@ export function BoardAdd() {
     setProgress(true);
 
     axios
-      .post("/api/board/add", { title, content })
+      .postForm("/api/board/add", { title, content })
       .then((res) => res.data)
       .then((data) => {
         const message = data.message;
@@ -45,6 +46,17 @@ export function BoardAdd() {
   // 원래 이름과 사용하려는 변수명이 같으면 생략 가능
 
   const disabled = !(title.trim().length > 0 && content.trim().length > 0);
+
+  // files 의 파일명을 component 리스트로 만들기
+  const filesList = [];
+  for (const file of files) {
+    filesList.push(
+      <li>
+        {file.name}({Math.floor(file.size / 1024)} KB)
+      </li>,
+    );
+  }
+
   return (
     <Box>
       <h2>게시물 작성</h2>
@@ -58,7 +70,15 @@ export function BoardAdd() {
             onChange={(e) => setContent(e.target.value)}
           />
         </Field>
-
+        <Box>
+          <Input
+            type={"file"}
+            onChange={(e) => setFiles(e.target.files)}
+            accept={"image/*"}
+            multiple
+          />
+          <Box>{filesList}</Box>
+        </Box>
         <Box>
           <Button
             disabled={disabled}
