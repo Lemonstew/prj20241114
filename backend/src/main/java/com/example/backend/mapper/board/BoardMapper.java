e09ac9de0f890a7a63ebda79b47e93b6d59e4dcb
Lemonstew <colt4949@naver.com>
2024-11-21 15:40:41 +0900

    파일과 댓글 있는 게시물 지우기

--- backend/src/main/java/com/example/backend/mapper/board/BoardMapper.java
+++ backend/src/main/java/com/example/backend/mapper/board/BoardMapper.java
@@ -82,4 +82,10 @@ public interface BoardMapper {
             WHERE board_id=#{id}
             """)
     List<String> selectFilesByBoardId(int id);
+
+    @Delete("""
+            DELETE FROM board_file
+            WHERE board_id=#{id}
+            """)
+    int deleteFileByBoardId(int id);
 }
