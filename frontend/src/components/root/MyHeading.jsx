abec7151686ebc80d718abe8131b99b5e02297ec
Lemonstew <colt4949@naver.com>
2024-11-25 10:12:24 +0900

    git ignore

--- frontend/src/components/root/MyHeading.jsx
+++ frontend/src/components/root/MyHeading.jsx
@@ -1,6 +1,6 @@
 import { Heading } from "@chakra-ui/react";
 
-export function MyHeading(children, ...rest) {
+export function MyHeading({ children, ...rest }) {
   return (
     <Heading size={{ base: "xl", md: "2xl" }} mb={7} {...rest}>
       {children}
